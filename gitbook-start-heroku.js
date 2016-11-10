dae382c63930e805575798eb8dc621d24a4d9ed6
Moises Yanes Carballo <alu0100782851@ull.edu.es>
2016-11-10 15:55:44 +0000

    Cambiando initialize

--- gitbook-start-heroku.js
+++ gitbook-start-heroku.js
@@ -13,44 +13,22 @@ function initialize(directorio) {
         '\n\tvar heroku = require("gitbook-start-heroku-alex-moi");'+
         '\n\tvar url = paquete.repository.url;'+
         
-        '\n\n\ heroku.deploy();'+
+        '\n\n\theroku.deploy();'+
         '\n});\n\n';
     
     
     //añadimos la tarea
-    fs.writeFileSync(path.resolve(__dirname, 'node_modules','gitbook-start-alex-moi-nitesh','gulpfile.js'), contenido,  {'flag':'a'},  function(err) {
+    fs.writeFileSync(path.resolve(process.cwd(),'gulpfile.js'), contenido,  {'flag':'a'},  function(err) {
         if (err) {
             return console.error(err);
         }
     });
-    
-    //copiamos gulpfile a nuestro directorio
-    fs.copyFile(path.resolve(__dirname, 'node_modules','gitbook-start-alex-moi-nitesh','gulpfile.js'), path.join(process.cwd(), 'gulpfile.js'),function(err){
-        if(err)
-          console.log(err);
-    });
-    
-    
-    fs.copyFile(path.resolve(__dirname, 'node_modules','gitbook-start-alex-moi-nitesh','template','Procfile'), path.join(process.cwd(), 'Procfile'),function(err){
-        if(err)
-          console.log(err);
-    });
-    
-     fs.copyFileSync(path.resolve(__dirname, 'node_modules','gitbook-start-alex-moi-nitesh','template','.env'), path.join(process.cwd(), '.env'),function(err){
-        if(err)
-          console.log(err);
-      });
-    
 };
 
 function deploy() {
 
-    
-
     console.log("Comenzando el deploy en HEROKU");
    
-    
-    
     child.exec('git add . ; git commit -m "subiendo a heroku"; git push heroku master;', function(error, stdout, stderr){
         if(error)
           console.log(error)
@@ -59,8 +37,6 @@ function deploy() {
         console.log(stdout);
       });
 
-
-   
 };
 
 module.exports = {
